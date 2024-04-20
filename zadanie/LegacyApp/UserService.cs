3d8240152c5bfc89d294fde24c382e7fbb910262
s24624 <s24624@pjwstk.edu.pl>
2024-04-20 19:55:45 +0200

    finalna refaktoryzacja kodu

--- zadanie/LegacyApp/UserService.cs
+++ zadanie/LegacyApp/UserService.cs
@@ -1,25 +1,25 @@
 ﻿using System;
-using System.Runtime.InteropServices.JavaScript;
 using System.Text.RegularExpressions;
 
 namespace LegacyApp
 {
     public class UserService
-    
+
     {
-        public bool validateNameAndSurname(string firstName,string lastName)
+        public bool validateNameAndSurname(string firstName, string lastName)
         {
             if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
             {
                 return false;
             }
+
             return true;
         }
 
-        static bool IsValidEmail(string email)
+        public bool isValidEmail(string email)
         {
             string pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
-            
+
             return Regex.IsMatch(email, pattern);
         }
 
@@ -31,42 +31,28 @@ namespace LegacyApp
             {
                 age--;
             }
+
             return age;
         }
 
         public bool checkAge(DateTime dateOfBirth)
         {
-            if (getAge(dateOfBirth)<21)
+            if (getAge(dateOfBirth) < 21)
             {
                 return false;
             }
 
             return true;
         }
-        
-        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+
+        public void checkClientType(ClientRepository clientRepository, User user, int clientId)
         {
-          //validate name
-          //validate e mail
-          //check age
-
-            var clientRepository = new ClientRepository();
             var client = clientRepository.GetById(clientId);
-
-            var user = new User
-            {
-                Client = client,
-                DateOfBirth = dateOfBirth,
-                EmailAddress = email,
-                FirstName = firstName,
-                LastName = lastName
-            };
-
-            if (client.Type == "VeryImportantClient")
+            if (client.Type.Equals("VeryImportantClient"))
             {
                 user.HasCreditLimit = false;
             }
-            else if (client.Type == "ImportantClient")
+            else if (client.Type.Equals("ImportantClient"))
             {
                 using (var userCreditService = new UserCreditService())
                 {
@@ -84,14 +70,48 @@ namespace LegacyApp
                     user.CreditLimit = creditLimit;
                 }
             }
+        }
 
+        public bool checkCreditLimit(User user)
+        {
             if (user.HasCreditLimit && user.CreditLimit < 500)
             {
                 return false;
             }
 
-            UserDataAccess.AddUser(user);
             return true;
         }
+
+        public User createUser(string firstName, string lastName, string email, DateTime dateOfBirth, Client client)
+        {
+            var user = new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                FirstName = firstName,
+                LastName = lastName
+            };
+            return user;
+        }
+
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (validateNameAndSurname(firstName, lastName) && isValidEmail(email) && checkAge(dateOfBirth))
+            {
+                var clientRepository = new ClientRepository();
+                var client = clientRepository.GetById(clientId);
+                var user = createUser(firstName, lastName, email, dateOfBirth, client);
+
+                checkClientType(clientRepository, user, clientId);
+                if (!checkCreditLimit(user)) return false;
+
+                UserDataAccess.AddUser(user);
+                return true;
+            }
+
+            return false;
+        }
     }
+
 }
