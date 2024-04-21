2a4ba2dc48726b4ff13e4921e03040ba04d9747c
s24624 <s24624@pjwstk.edu.pl>
2024-04-21 17:22:00 +0200

    dodano test metody adduser - czy imie nie jest pustym stringiem

--- zadanie/Tests/UserServiceTests.cs
+++ zadanie/Tests/UserServiceTests.cs
@@ -19,4 +19,19 @@ public class UserServiceTests
         
         Assert.Equal(false,result);
     }
+    [Fact]
+    public void addUser_should_return_false_if_firstName_is_an_empty_string()
+    {
+        string firstName = " ",
+            lastName = "Doe",
+            email = "incorrect";
+        DateTime dateBirth = DateTime.Parse("1982-03-21");
+        int clinetId = 1;
+        var service = new UserService();
+        
+        bool result = service.AddUser(firstName, lastName, email, dateBirth, clinetId);
+        
+        Assert.Equal(false,result);
+    }
+    
 }
