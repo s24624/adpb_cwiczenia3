637eba9fc440c717749b1699ea64c1e0b1057820
s24624 <s24624@pjwstk.edu.pl>
2024-04-21 17:13:04 +0200

    Dodano pierwszy test metody addUser - poprawnosc email

---
+++ zadanie/LegacyAppTests/UserServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+
+namespace LegacyAppTests
+{
+    public class UserServiceTest
+    {
+        [Fact]
+        public void addUser_should_return_false_when_email_is_not_correct()
+        {
+            string firstName = "John",
+                lastName = "Doe",
+                email = "incorrect";
+            DateTime birthDate = DateTime.Parse("1982-03-21");
+            int clinetId = 1;
+            var userService = new UserService();
+            
+            bool result = user
+            Assert.True(true);
+        }
+    }
+}
---
+++ zadanie/Tests/UserServiceTests.cs
@@ -0,0 +1,22 @@
+using System.Runtime.InteropServices.JavaScript;
+using LegacyApp;
+
+namespace Tests;
+
+public class UserServiceTests
+{
+    [Fact]
+    public void addUser_should_return_false_if_email_is_incorrect()
+    {
+        string firstName = "John",
+            lastName = "Doe",
+            email = "incorrect";
+        DateTime dateBirth = DateTime.Parse("1982-03-21");
+        int clinetId = 1;
+        var service = new UserService();
+
+        bool result = service.AddUser(firstName, lastName, email, dateBirth, clinetId);
+        
+        Assert.Equal(false,result);
+    }
+}
